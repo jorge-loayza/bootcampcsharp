11d56b06a960a1956b55e9162db8f91d6bb07be4
Jorge Loayza <Jorge.Loayza@jala-foundation.org>
2021-07-01 18:02:04 -0400

    Classes Exercise

---
+++ ClassesEercise/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toyota toyota = new Toyota(150.00M, 6, 4);
+            ElectricFlyingCar flyingCar = new ElectricFlyingCar(250.00M, 4,4);
+            Boat boat = new Boat(200.00M, 2);
+            Boeing boeing = new Boeing(200.00M, 200, 15000.00M);
+
+            //Prit all data and call methods
+            Console.WriteLine(toyota);
+            toyota.Handle();
+            Console.WriteLine(flyingCar);
+            flyingCar.Fly();
+            flyingCar.Land();
+            Console.WriteLine(boat);
+            boat.Navigate();
+            Console.WriteLine(boeing);
+            boeing.Fly();
+            boeing.Land();
+        }
+    }
+    public class Transport
+    {
+        public decimal MaxSpeed { get; set; }
+        public int Capacity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}\n Max Speed: {this.MaxSpeed}\n Capacity: {this.Capacity}\n";
+        }
+
+    }
+
+    public interface IFly
+    {
+        public void Fly();
+        public void Land();
+    }
+    public class AirTransport : Transport, IFly
+    {
+        public decimal MaximunHeight { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"> {this.GetType().Name} is flying.\n");
+        }
+
+        public void Land()
+        {
+            Console.WriteLine($"> {this.GetType().Name} is landing.\n");
+        }
+        public override string ToString()
+        {
+            return string.Concat(base.ToString(), $" Maximum Height: {this.MaximunHeight}\n") ;
+        }
+    }
+    public class LandTransport : Transport
+    {
+        public int Wheels { get; set; }
+        public void Handle()
+        {
+            Console.WriteLine($"{this.GetType().Name} is being handle.\n");
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(base.ToString(), $" Wheels: {this.Wheels}\n") ;
+        }
+    }
+    public class AquaticTransport : Transport
+    {
+        public void Navigate()
+        {
+            Console.WriteLine($"> {this.GetType().Name} is navigating.\n");
+        }
+    }
+
+    public class Toyota : LandTransport
+    {
+        public Toyota(decimal maxVel, int capacity, int wheels)
+        {
+            base.MaxSpeed = maxVel;
+            base.Capacity = capacity;
+            base.Wheels = wheels;
+        }
+    }
+
+    public class ElectricFlyingCar : LandTransport, IFly
+    {
+
+        public ElectricFlyingCar(decimal maxVel, int capacity, int wheels)
+        {
+            base.MaxSpeed = maxVel;
+            base.Capacity = capacity;
+            base.Wheels = wheels;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"> {this.GetType().Name} is flying.\n");
+        }
+
+        public void Land()
+        {
+            Console.WriteLine($"> {this.GetType().Name} is landing.\n");
+        }
+    }
+
+    public class Boat: AquaticTransport
+    {  
+        public Boat(decimal maxVel, int capacity)
+        {
+            base.MaxSpeed = maxVel;
+            base.Capacity = capacity;
+        }
+    }
+    public class Boeing : AirTransport
+    {
+     
+
+        public Boeing(decimal maxVel, int capacity, decimal maxHeight)
+        {
+            base.MaxSpeed = maxVel;
+            base.Capacity = capacity;
+            base.MaximunHeight = maxHeight;
+        }
+    }
+}
---
+++ InterfacesClasses/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfacesClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Person[] people =
+            //{
+            //    new Person{ Name = "Simon"},
+            //    new Person{ Name = "Pepe"},
+            //    new Person{ Name = "Adam"},
+            //    new Person{ Name = "Richard"},
+            //    new Person{ Name = "Maria"},
+            //};
+
+            //foreach (var item in people)
+            //{
+            //    Console.WriteLine($"{item.Name}");
+            //}
+
+            ////Array.Sort(people);
+            //Array.Sort(people, new PersonComparer());
+            //Console.WriteLine("\n\nSorted");
+
+            //foreach (var item in people)
+            //{
+            //    Console.WriteLine($"{item.Name}");
+            //}
+
+
+            //IPlayable dvDPlayer = new MP3Player();
+
+            //dvDPlayer.Stop();
+
+            Employee juan = new Employee
+            {
+                Name = "Juan",
+                DateOfBirth = new DateTime(1990,7,28)
+            };
+
+            juan.EmplyeeCode = "111a";
+            juan.HireDate = new DateTime(2014, 11, 20);
+            Console.WriteLine($"{juan.Name} was gired on {juan.HireDate:dd/MM/yy}");
+            //juan.WriteInformation();
+            Console.WriteLine(juan);
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string EmplyeeCode { get; set; }
+        public DateTime HireDate { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} and hired on { HireDate:dd/MM/yy}";
+        }
+    }
+    public sealed class MysealedClass
+    {
+
+    }
+
+    public class TesT
+    {
+
+    }
+
+    public class Singer
+    {
+        public virtual void Sing()
+        {
+            Console.WriteLine("Singing...");
+        }
+    }
+    public class LadyGaga: Singer
+    {
+        public sealed override void Sing()
+        {
+            Console.WriteLine("Singing with style...");
+        }
+    }
+    //public class TestDinger : LadyGaga
+    //{
+    //    public sealed override void Sing()
+    //    {
+    //        Console.WriteLine("Singing with style...");
+    //    }
+    //}
+    public class DvDPlayer : IPlayable
+    {
+        public void Pause()
+        {
+            Console.WriteLine("DVD player is in pause.");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("DVD payer is playing...");
+        }
+    }
+    public class MP3Player : IPlayable
+    {
+        public void Pause()
+        {
+            Console.WriteLine("DVD player is in pause.");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("DVD payer is playing...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping");
+        }
+    }
+
+    public interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop()
+        {
+            Console.WriteLine("Default implementation for stop.");
+        }
+    }
+
+    //class Person: IComparable<Person>
+    //{
+    //    public string Name {get; set;}
+
+    //    public int CompareTo(Person otherPerson)
+    //    {
+    //        return Name.CompareTo(otherPerson.Name);
+    //    }
+    //}
+    // From another dll
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} was born on a {DateOfBirth.DayOfWeek}";
+        }
+    }
+
+    class PersonComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
